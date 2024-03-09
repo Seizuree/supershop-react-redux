c14eea335fc2da77f2e5c764313657624b4cd92f
Brian <briangiovanni03@gmail.com>
2024-03-09 16:09:14 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './containers/Header'
+import ProductListing from './containers/ProductListing'
+import ProductDetail from './containers/ProductDetail'
+
+function App() {
+  return (
+    <div className=''>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing/>} />
+          <Route path="/product/:productId" element={<ProductDetail/>} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
